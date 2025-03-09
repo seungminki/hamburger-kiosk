4d2de5d311aefd9738e98f728d4c0046e9021f5e
Luji <fsjki0328@naver.com>
2025-03-10 00:24:33 +0900

    Feat: Product Dao (Data access object) 클래스 추가

---
+++ src/main/entities/ProductDao.java
@@ -0,0 +1,71 @@
+package main.entities;
+
+import main.ExtractMd;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class ProductDao {
+    private Map<String, Product> products = DatabaseClass.getProduct();
+
+    public void addProduct() throws IOException {
+        ExtractMd extractMd = new ExtractMd();
+
+        List<String> lines = extractMd.readMdFiles();
+        List<Product> productList = extractMd.alterDataLines(lines);
+
+        for (Product product : productList) {
+            products.put(product.getName(), new Product(product.getName(), product.getPrice(), product.getQuantity(), product.getDescription(), product.getCategory()));
+
+        }
+    }
+
+    public List<Product> getAllProducts() {
+        return new ArrayList<>(products.values());
+
+    }
+
+    public Product getProduct(String productName) {
+        return products.get(productName);
+    }
+
+
+    public void updateProduct(String productName, int productCount) {
+        Product product = getProduct(productName);
+
+        int currentQuantity = Integer.parseInt(product.getQuantity());
+
+        Product updatedProduct = new Product(
+                product.getName(),
+                product.getPrice(),
+                String.valueOf(currentQuantity - productCount),
+                product.getDescription(),
+                product.getCategory()
+        );
+
+        products.put(productName, updatedProduct);
+
+    }
+
+
+    public List<Product> getProductByCategory(String category) {
+
+        List<Product> productList = new ArrayList<>();
+
+        for (Product p : this.products.values()) {
+            if (p.getCategory().equals(category)) {
+                productList.add(p);
+            }
+        }
+
+        for (Product p : productList) {
+            System.out.println(p);
+        }
+
+        return productList;
+
+
+    }
+}

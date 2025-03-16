0c4f1dfb415706b720b2c7b5b3b6b449a33910e8
Luji <fsjki0328@naver.com>
2025-03-17 01:28:07 +0900

    Chore: 상수 추출

--- src/main/Input.java
+++ src/main/Input.java
@@ -5,34 +5,25 @@ import java.util.Scanner;
 public class Input {
     Scanner sc = new Scanner(System.in);
 
-    public int getMenuInput() {
-        int input = sc.nextInt();
+    private static final String ACCOUNT_INPUT_DELIMITER = " ";
+    private static final String PRODUCT_INPUT_REGEX = "[\\[\\]]";
+    private static final String EMPTY_STRING = "";
+    private static final String INPUT_DELIMITER = ",";
 
-        return input;
+    public int getMenuInput() {
+        return sc.nextInt();
     }
 
     public String getInput() {
-        String input = sc.nextLine().trim();
-
-        return input;
+        return sc.nextLine().trim();
     }
-
-    public void close() {
-        sc.close();
-    }
-
 
     public String[] splitAccountInput(String input) {
-        String[] parts = input.replace(" ", "").split(",");
-        // TODO: Account + Assets 형태 아닐 시 에러
-        
-        return parts;
+        return input.replace(ACCOUNT_INPUT_DELIMITER, EMPTY_STRING).split(INPUT_DELIMITER);
     }
 
     public String[] splitProductInput(String input) {
-        String[] items = input.replaceAll("[\\[\\]]", "").split(",");
-
-        return items;
+        return input.replaceAll(PRODUCT_INPUT_REGEX, EMPTY_STRING).split(INPUT_DELIMITER);
     }
 
 }
